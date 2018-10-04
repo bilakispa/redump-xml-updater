8a8e53c330e18d2278f2e7a89d25680b7204902f
bilakispa <bilakispa@users.noreply.github.com>
2018-10-04 23:32:39 +0300

    Added crawling with user specific data

--- redump-xml-updater.py
+++ redump-xml-updater.py
@@ -1,54 +1,102 @@
 import sys
-from urllib.request import Request, urlopen
-import urllib.parse
-import socket
-import re
+import os
 import xml.etree.ElementTree as ET
+import re
+import requests
 import http.server
 import socketserver
 
 #Config
-url = 'http://redump.org/'
+userinfo_filename = 'UserInfo.xml'
+url_main = 'http://redump.org/'
+url_login = 'http://forum.redump.org/login/'
 regex = {
 	'datfile' : r'<a href="/datfile/(.*?)">',
 	'date' : r'\) \((.*?)\)\.',
-	'name' : r'filename="(.*?) Datfile'
+	'name' : r'filename="(.*?) Datfile',
+	'csrf' : r'csrf_token" value="(.*?)"'
 }
+
 xml_filename = 'profile.xml'
 server_address = '127.0.0.1'
 server_port = 80
 
 def Update_XML():
-	print('Updating XML ...')
+	print('Updating XML:')
 	
 	print(' * Retrieving site info', end=' ')
 	sys.stdout.flush()
-	try:
-		response = urlopen(url, timeout=30)
-	except urllib.error.URLError:
-		print('URL Error')
-		quit()
-	except socket.timeout:
-		print('Socket timeout')
-		quit()
-
-	respData = response.read()
+	
+	#Get user info
+	doLogin = 0
+	username = ''
+	password = ''
+	if os.path.exists(userinfo_filename):
+		#Check if is empty
+		try:    
+			tree = ET.parse(userinfo_filename)
+			root = tree.getroot()
+			username = root[0].text
+			password = root[1].text
+			if (username != None and password != None):
+				doLogin = 1
+		except ET.ParseError: #XML file is empty
+			pass
+	
+	client = requests.session()
+	if (doLogin == 1):
+		#Retrieve the csrf token
+		try:
+			result = client.get(url_login)
+		except requests.exceptions.RequestException as e:
+			print ('Error: ')
+			print (e)
+			sys.exit(1)
+		
+		csrf_token = re.findall(regex['csrf'], str(result.text))[0]
+		
+		#Send a POST request to login
+		payload = {
+			'form_sent' : '1',
+			'redirect_url' : url_main,
+			'csrf_token' : csrf_token,
+			'req_username' : username,
+			'req_password' : password,
+			'save_pass' : '0',
+			'login' : 'Login'
+		}
+	
+		header = {'User-Agent': 'Mozilla/5.0', 'referer': 'http://forum.redump.org/login/'}
+		try:
+			result = client.post(url_login, data=payload, headers=header)
+		except requests.exceptions.RequestException as e:
+			print ('Error: ')
+			print (e)
+			sys.exit(1)
+	else:
+		try:
+			result = client.get(url_main)
+		except requests.exceptions.RequestException as e:
+			print ('Error: ')
+			print (e)
+			sys.exit(1)
 	
 	print('(DONE)')
 	print(' * Processing data', end=' ')
 	sys.stdout.flush()
 	
-	datFiles = re.findall(regex['datfile'], str(respData))
+	datFiles = re.findall(regex['datfile'], str(result.text))
 	datInfo = []
 	dict = {}
 	for dat in datFiles:
-		response = urlopen(Request(url+'datfile/'+dat, method='HEAD'))
-		header = response.info()
+		response = client.head(url_main+'datfile/'+dat)
+		content_header = response.headers['Content-Disposition']
+		
 		#get the date from the file name
-		dict['Date'] = re.findall(regex['date'], header['Content-Disposition'])[0]
+		dict['Date'] = re.findall(regex['date'], content_header)[0]
 		
 		#generate the dat's name
-		tempName = re.findall(regex['name'], header['Content-Disposition'])[0]
+		tempName = re.findall(regex['name'], content_header)[0]
 		#trim the - from the end (if exists)
 		if (tempName.endswith('-')):
 			tempName = tempName[:-2]
@@ -78,6 +126,7 @@ def Update_XML():
 	print('(DONE)')
 
 def Start_Server():
+	print('Starting Local Server:')
 	Handler = http.server.SimpleHTTPRequestHandler
 	with socketserver.TCPServer((server_address, server_port), Handler) as httpd:
 		print('Serving HTTP on ' + server_address + ' port ' + str(server_port) + ' (http://'+server_address+':'+str(server_port)+'/) ...')
@@ -90,12 +139,12 @@ def Start_Server():
 			# Clean-up server (close socket, etc.)
 			httpd.server_close()
 
-print('Welcome to Redump.org XML updater')
+print('Welcome to Redump.org XML updater [Github: bilakispa/redump-xml-updater]')
 try:
 	option = input('1: Update XML & Run Local Server\n2: Update XML\n3: Run Local Server\n')
 	while(option != '1' and option != '2' and option != '3' and option != ''):
 		option = input('Please select a valid option [1,3] (Press enter to exit): ')
-		
+	
 	if(option == '1'):
 		Update_XML()
 		Start_Server()
