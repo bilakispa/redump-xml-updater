b5a3fc6e1b1361bb1e485b929594555b2286bce4
bilakispa <bilakispa@users.noreply.github.com>
2021-07-03 17:58:05 +0300

    Updated script to include file url inside the xml, change the port on the local webserver

--- redump-xml-updater.py
+++ redump-xml-updater.py
@@ -8,23 +8,23 @@ import socketserver
 
 #Config
 userinfo_filename = 'UserInfo.xml'
-url_home = 'http://redump.org/'
+url_datfile = 'http://redump.org/datfile/'
 url_downloads = 'http://redump.org/downloads/'
 url_login = 'http://forum.redump.org/login/'
 regex = {
 	'datfile' : r'<a href="/datfile/(.*?)">',
 	'date' : r'\) \((.*?)\)\.',
+	'filename' : r'filename="(.*?)"',
 	'name' : r'filename="(.*?) Datfile',
 	'csrf' : r'csrf_token" value="(.*?)"'
 }
 
 xml_filename = 'profile.xml'
 server_address = '127.0.0.1'
-server_port = 80
+server_port = 8668
 
 def Update_XML():
 	print('Updating XML:')
-	
 	print(' * Retrieving site info', end=' ')
 	sys.stdout.flush()
 	
@@ -67,7 +67,7 @@ def Update_XML():
 			'login' : 'Login'
 		}
 	
-		header = {'User-Agent': 'Mozilla/5.0', 'referer': 'http://forum.redump.org/login/'}
+		header = {'User-Agent': 'Mozilla/5.0', 'referer': url_login}
 		try:
 			result = client.post(url_login, data=payload, headers=header)
 		except requests.exceptions.RequestException as e:
@@ -90,9 +90,9 @@ def Update_XML():
 	datInfo = []
 	dict = {}
 	for dat in datFiles:
-		response = client.head(url_home+'datfile/'+dat)
+		response = client.head(url_datfile + dat)
 		content_header = response.headers['Content-Disposition']
-		
+
 		#get the date from the file name
 		dict['Date'] = re.findall(regex['date'], content_header)[0]
 		
@@ -104,6 +104,16 @@ def Update_XML():
 		elif (tempName.endswith('BIOS')):
 			tempName = tempName + ' Images'
 		dict['Name'] = tempName
+
+		dict['URL'] = url_datfile + dat
+		
+		filename = re.findall(regex['filename'], content_header)[0]
+		if (filename.endswith('.zip')):
+			filename = filename[:-3] + 'dat' # replace with .dat
+		else:
+			filename = ''
+		dict['File'] = filename
+
 		datInfo.append(dict.copy())
 	
 	print('(DONE)')
@@ -117,9 +127,9 @@ def Update_XML():
 		ET.SubElement(tag_datfile, 'description').text = info['Name']
 		ET.SubElement(tag_datfile, 'version').text = info['Date']
 		ET.SubElement(tag_datfile, 'author').text = 'redump.org'
-		ET.SubElement(tag_datfile, 'comment').text = '_'
-		ET.SubElement(tag_datfile, 'url').text = '_'
-		ET.SubElement(tag_datfile, 'file').text = '_'
+		ET.SubElement(tag_datfile, 'comment').text = ''
+		ET.SubElement(tag_datfile, 'url').text = info['URL']
+		ET.SubElement(tag_datfile, 'file').text = info['File']
 		
 	xmldata = ET.tostring(tag_clrmamepro).decode()
 	xmlfile = open(xml_filename, 'w')
@@ -131,13 +141,13 @@ def Start_Server():
 	Handler = http.server.SimpleHTTPRequestHandler
 	with socketserver.TCPServer((server_address, server_port), Handler) as httpd:
 		print('Serving HTTP on ' + server_address + ' port ' + str(server_port) + ' (http://'+server_address+':'+str(server_port)+'/) ...')
-		#httpd.serve_forever()
 		try:
 			httpd.serve_forever()
 		except KeyboardInterrupt:
 			print('Terminating server ...')
 		finally:
 			# Clean-up server (close socket, etc.)
+			httpd.shutdown()
 			httpd.server_close()
 
 print('Welcome to Redump.org XML updater [Github: bilakispa/redump-xml-updater]')
